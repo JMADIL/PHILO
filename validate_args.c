fcd21535858670f591e02a821954e1e6039bd42d
Adil Jamoun <ajamoun@c1r7p8.1337.ma>
2025-06-11 23:47:24 +0100

    PHILO

--- validate_args.c
+++ validate_args.c
@@ -2,9 +2,12 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   validate_args.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ajamoun <ajamoun@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
+/*                                                    +:+ +:+        
+	+:+     */
+/*   By: ajamoun <ajamoun@student.42.fr>            +#+  +:+      
+	+#+        */
+/*                                                +#+#+#+#+#+  
+	+#+           */
 /*   Created: 2025/06/10 20:14:59 by ajamoun           #+#    #+#             */
 /*   Updated: 2025/06/10 20:14:59 by ajamoun          ###   ########.fr       */
 /*                                                                            */
@@ -12,16 +15,37 @@
 
 #include "philo.h"
 
-int validate_args(int ac, char **av)
+static int	ft_isdigit(char *str)
+{
+	int i;
 
-    int i;
-    int num_of_philos;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] > 9 || str[i] < 0)
+			return (EXIT_FAILURE);
+		i++;
+	}
+	return (EXIT_SUCCESS);
+}
 
-    num_of_philos = ft_atoi(av[0]);
-    if(num_of_philos > 200 || num_of_philos <= 0)
-    {
-        error_message("Error: Number of philosophers must be between 1 and 200.\n");
-        return (EXIT_FAILURE);
-    }
+int	validate_args(int ac, char **av)
+{
+	int i;
+	int num_of_philos;
 
-
+	num_of_philos = ft_atoi(av[0]);
+	if (num_of_philos > 200 || num_of_philos <= 0)
+	{
+		error_message("Error: Number of philosophers must be between 1 and 200.\n");
+		return (EXIT_FAILURE);
+	}
+	i = 1;
+	while (i < argc)
+	{
+		if (av[i][0] == '\0' || ft_isdigit(av[i]) || ft_atoi(av[i]) == -1)
+			return (EXIT_FAILURE);
+		i++;
+	}
+	return (EXIT_SUCCESS);
+}
