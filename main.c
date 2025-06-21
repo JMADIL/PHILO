b868d888ebd10bad23e32a0607decb2c747414a7
Adil Jamoun <ajamoun@c1r7p2.1337.ma>
2025-06-21 22:49:57 +0100

    PHILO

--- main.c
+++ main.c
@@ -12,6 +12,10 @@
 
 #include "philo.h"
 
+int init_data(char *av[], t_data *data, int must_eats)
+{
+
+}
 static int  philos_checker(t_data *data, t_philo **philos, int ac, char **av)
 {
     if(validate_args(ac - 1, av + 1))
@@ -19,6 +23,12 @@ static int  philos_checker(t_data *data, t_philo **philos, int ac, char **av)
         error_message("Error: At least one argument is not valid.\n");
 		return (EXIT_FAILURE);
     }
+    if(init_data() || init_philos())
+    {
+        error_message("Error: Malloc failed.\n");
+        return (EXIT_FAILURE);
+    }
+    return (EXIT_SUCCESS);
 }
 int main(int ac, char **av)
 {
@@ -27,6 +37,6 @@ int main(int ac, char **av)
         error_message("Error: Invalid number of arguments, Expected 5 or 6.\n");
         return (EXIT_FAILURE);
     }
-    if(ac == 6 && atoi(av[5]) < 0)
+    if(ac == 6 && ft_atoi(av[5]) < 0)
         return (EXIT_SUCCESS);
 }
