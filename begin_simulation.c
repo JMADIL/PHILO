2badb11d2f81e92f250030811291e6e50461b1b5
Adil Jamoun <ajamoun@c1r3p6.1337.ma>
2025-07-13 22:46:46 +0100

    PHILO

--- begin_simulation.c
+++ begin_simulation.c
@@ -3,15 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   begin_simulation.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ajamoun <ajamoun@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/12 18:37:14 by ajamoun           #+#    #+#             */
-/*   Updated: 2025/07/12 18:37:14 by ajamoun          ###   ########.fr       */
+/*   Created: 2025/07/13 16:13:02 by ajamoun           #+#    #+#             */
+/*   Updated: 2025/07/13 16:13:02 by ajamoun          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
 
+void *routine(void *arg)
+{
+    t_philo *philos = (t_philo *)arg;
+    if(philos->data->nbr_of_philos == 1)
+       return --->>> one_fork(philos); <<<---
+    
+
+}
 int begin_simulation(t_philo *philos)
 {
     int i;
@@ -20,6 +28,17 @@ int begin_simulation(t_philo *philos)
     philos->data->start_time = --->>> get_current_time() <<<---;
     while(++i < philos->data->nbr_of_philos)
     {
-        
+        philos[i].last_meal_time = philos->data->start_time;
+        if(pthread_create(&philos[i].thread, NULL, routine, &philos[i]) != '\0')
+        {
+            error_message("Error: Failed to create threads.\n");
+            return (EXIT_FAILURE);
+        }
     }
+    if(philo->data->nbr_of_philos > 1)
+        --->>> monitor_death(); <<<---
+    i = -1;
+    while(++i < philo->data->nbr_of_philos)
+        pthread_join(&philos[i].thread, NULL);
+    return (EXIT_SUCCESS);
 }
--- philo_utils.c
+++ philo_utils.c
@@ -2,9 +2,12 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   philo_utils.c                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ajamoun <ajamoun@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
+/*                                                    +:+ +:+        
+	+:+     */
+/*   By: ajamoun <ajamoun@student.42.fr>            +#+  +:+      
+	+#+        */
+/*                                                +#+#+#+#+#+  
+	+#+           */
 /*   Created: 2025/06/11 21:18:06 by ajamoun           #+#    #+#             */
 /*   Updated: 2025/06/11 21:18:06 by ajamoun          ###   ########.fr       */
 /*                                                                            */
@@ -14,8 +17,9 @@
 
 int	error_message(char *error_message)
 {
-    int len;
-    len = ft_strlen(error_message) + 1;
-    write(2, error_message, len);
-    return (len);
+	int	len;
+
+	len = ft_strlen(error_message) + 1;
+	write(2, error_message, len);
+	return (len);
 }
